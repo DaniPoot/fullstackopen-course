f859a58c4737395111e01bd0ad89bfc6ad20b0af
DaniPoot <danielpoot.uc2@gmail.com>
2023-02-06 21:10:40 -0600

    Finish exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sumOfLikes = (prev, blog) => prev + (blog?.likes || 0)
+  return blogs.reduce(sumOfLikes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
