5190d628fdc6eb230bcd086cb4ab4fdb24966dcf
DaniPoot <danielpoot.uc2@gmail.com>
2023-02-11 11:39:33 -0600

    Finish exercise 4.8, step 1

---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,24 @@
+const initialBlogs = [
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 12,
+    url: ''
+  },
+  {
+    title: "It ends with us",
+    author: "Colleen Hoover",
+    likes: 1,
+    url: ''
+  },
+  {
+    title: 'It starts with us',
+    author: 'Colleen Hoover',
+    likes: 85,
+    url: ''
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
