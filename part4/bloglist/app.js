96cf97ad79b38f899e75f06f45d19e81d9370808
DaniPoot <danielpoot.uc2@gmail.com>
2023-02-16 21:22:48 -0600

    Finish exercise 4.15, step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,12 +5,14 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
